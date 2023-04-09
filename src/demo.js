0bbe981d9f7d6f6b0652f35a83c4556965082174
baishun <863548227@qq.com>
2023-04-09 09:11:57 +0800

    feat:初始化脚手架

--- src/demo.js
+++
@@ -1,3 +0,0 @@
-export function add2(a, b) {
-  return a + b;
-}
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import { createApp, h } from 'vue'
-import { add2 } from './demo.js'
-console.log(add2(1,2));
-// createApp('div',null,[h('div',null,'hello world')]).mount('#root')
